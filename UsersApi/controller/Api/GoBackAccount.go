88dbdea8fc76d4824e50cbac582328ffd9b207b1
sunyuting83 <sunyuting83@gmail.com>
2023-03-26 23:53:38 +0800

    first commit

--- UsersApi/controller/Api/GoBackAccount.go
+++ UsersApi/controller/Api/GoBackAccount.go
@@ -10,45 +10,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type StatusJSON struct {
-	Status   string `json:"status"`
-	Title    string `json:"title"`
-	Delete   bool   `json:"delete"`
-	CallBack bool   `json:"callback"`
-	BackTo   string `json:"backto"`
-	Export   bool   `json:"export"`
-	Import   bool   `json:"import"`
-	Pull     bool   `json:"pull"`
-}
-
-func BackToAccount(c *gin.Context) {
+func GoBackAccount(c *gin.Context) {
 	var (
-		computid string = c.Query("computid")
-		gameid   string = c.Query("gameid")
-		status   string = c.Query("status")
-		IsJson   string = c.DefaultQuery("json", "0")
+		status string = c.Query("status")
+		IsJson string = c.DefaultQuery("json", "0")
 	)
-	if len(computid) == 0 {
-		if IsJson == "1" {
-			c.JSON(http.StatusOK, gin.H{
-				"status":  1,
-				"message": "haven't params found",
-			})
-			return
-		}
-		c.String(200, "参数错误")
-		return
-	}
 	Path := c.Request.URL.Path
 	PathList := strings.Split(Path, "/")
 	Path = PathList[len(PathList)-1]
 	if strings.Contains(Path, "clean") {
 		status = GetBackPath(Path)
 	}
-	if gameid == "14e7110dd307" {
-		status = "9"
-	}
-	if len(status) == 0 {
+	if !strings.Contains(Path, "clean") && len(status) == 0 {
 		if IsJson == "1" {
 			c.JSON(http.StatusOK, gin.H{
 				"status":  1,
@@ -104,23 +77,9 @@ func BackToAccount(c *gin.Context) {
 		c.String(200, "状态不支持退回")
 		return
 	}
-
-	computs, err := database.GetOneComputer(computid)
-	if err != nil {
-		if IsJson == "1" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status":  1,
-				"message": "获取机器码失败",
-			})
-			return
-		}
-		c.String(200, "获取机器码失败")
-		return
-	}
-
 	backToStatusInt, _ := strconv.Atoi(backToStatus)
 	var account *database.Accounts
-	account.BackToAcc(projectsID, status, backToStatusInt, computs.ID)
+	account.BackTo(projectsID, status, backToStatusInt)
 	if IsJson == "1" {
 		c.JSON(http.StatusOK, gin.H{
 			"status":  0,
@@ -129,17 +88,4 @@ func BackToAccount(c *gin.Context) {
 		return
 	}
 	c.String(200, "退回成功")
-
-}
-
-func GetBackPath(s string) (status string) {
-	switch s {
-	case "cleanreg":
-		status = "1"
-	case "cleanpaly":
-		status = "3"
-	case "cleanplaycache":
-		status = "4"
-	}
-	return
 }
