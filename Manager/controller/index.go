bda0953b35a73ef6c2be5b2c071f1f2bdd72aa48
sunyuting83 <sunyuting83@gmail.com>
2023-05-30 13:44:51 +0800

    first commit

--- Manager/controller/index.go
+++ Manager/controller/index.go
@@ -1,11 +1,10 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
+	// c.HTML(http.StatusOK, "index.html", nil)
+	c.String(200, "ok")
 }
